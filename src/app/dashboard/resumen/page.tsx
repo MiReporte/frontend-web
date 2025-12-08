e9a13593eb887b354340bdd20cc8fc2580cfea2c
JosueRS22 <zS23017357@estudiantes.uv.mx>
2025-12-07 19:17:34 -0600

    Update page.tsx

--- src/app/dashboard/resumen/page.tsx
+++ src/app/dashboard/resumen/page.tsx
@@ -5,7 +5,7 @@ import ProtectedPage from "@/components/ProtectedPage";
 export default function ResumenPage() {
   return (
     <ProtectedPage permission="resumen">
-      <h1>Vista de Resumen</h1>
+      <h1>Vista de Resumen2</h1>
     </ProtectedPage>
   );
 }
